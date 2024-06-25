85e80407359b620885fed57e9cbe18de4671ec51
Arushad Ahmed <dash-8x@hotmail.com>
2024-06-25 20:50:51 +0500

    - Updated tests

--- tests/Feature/EfaasProviderTest.php
+++ tests/Feature/EfaasProviderTest.php
@@ -3,7 +3,6 @@
 namespace Javaabu\EfaasSocialite\Tests\Feature;
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Str;
 use Javaabu\EfaasSocialite\Tests\TestCase;
 use Javaabu\EfaasSocialite\Tests\TestSupport\Controllers\LoginController;
 
@@ -16,20 +15,14 @@ class EfaasProviderTest extends TestCase
 
         Route::get('/oauth/{socialite_provider}/callback', [LoginController::class, 'redirectToProvider'])->where('socialite_provider', 'efaas');
 
-        $nonce = 'R3ztkHyHgafKh1k445wPKzzTqXBxI3bTXZSKv1HX';
-
-        Str::createRandomStringsUsing(function () use ($nonce) {
-            return $nonce;
-        });
-
         $this->get('/oauth/efaas/callback')
-            ->assertRedirect(
+            ->assertRedirectContains(
                 'https://efaas.gov.mv/connect/authorize?'.
                 'client_id='.self::CLIENT_ID.
                 '&redirect_uri='. urlencode(self::REDIRECT_URL) .
                 '&response_type=' . urlencode('code id_token') .
                 '&response_mode=form_post'.
                 '&scope=' . urlencode('openid efaas.profile efaas.birthdate efaas.email efaas.mobile efaas.photo efaas.permanent_address efaas.country efaas.passport_number efaas.work_permit_status') .
-                '&nonce=' . $nonce);
+                '&nonce=');
     }
 }
