15b960ccd6fbce68849838e51292adb8dbd1402b
Arushad Ahmed <dash-8x@hotmail.com>
2024-06-25 23:13:21 +0500

    - Added test for one tap login

--- tests/Feature/EfaasProviderTest.php
+++ tests/Feature/EfaasProviderTest.php
@@ -31,4 +31,29 @@ class EfaasProviderTest extends TestCase
                 $redirect_url
         );
     }
+
+    /** @test */
+    public function it_can_perform_efaas_one_tap_login_redirect()
+    {
+        $this->withoutExceptionHandling();
+
+        $login_code = 'a5d9a8ac-d583-41a7-8844-545dd608fad7';
+
+        $response = $this->get('/efaas-one-tap-login?efaas_login_code=' . $login_code)
+            ->assertRedirect();
+
+        $redirect_url = $response->headers->get('Location');
+
+        $this->assertStringStartsWith(
+            'https://efaas.gov.mv/connect/authorize?'.
+            'client_id='.self::CLIENT_ID.
+            '&redirect_uri='. urlencode(self::REDIRECT_URL) .
+            '&response_type=' . urlencode('code id_token') .
+            '&response_mode=form_post'.
+            '&scope=' . urlencode('openid efaas.profile efaas.birthdate efaas.email efaas.mobile efaas.photo efaas.permanent_address efaas.country efaas.passport_number efaas.work_permit_status') .
+            '&acr_values=' . urlencode('efaas_login_code:' . $login_code) .
+            '&nonce=',
+            $redirect_url
+        );
+    }
 }
