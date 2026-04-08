d3eba7c2c6403c2898f03dbf7ce0e2563b935bba
Arushad Ahmed <dash-8x@hotmail.com>
2026-04-08 18:53:19 +0500

    Update 0001_01_01_000000_create_users_table.php

--- tests/TestSupport/database/0001_01_01_000000_create_users_table.php
+++ tests/TestSupport/database/0001_01_01_000000_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -46,4 +46,4 @@ return new class extends Migration
         Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
     }
-};
+}
