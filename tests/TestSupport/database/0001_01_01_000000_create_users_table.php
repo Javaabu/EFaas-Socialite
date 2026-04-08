715a2c0d5aca6a8f1a3ef851c6723ccdbc640ccc
Arushad Ahmed <dash-8x@hotmail.com>
2026-04-08 19:03:46 +0500

    Update 0001_01_01_000000_create_users_table.php

--- tests/TestSupport/database/0001_01_01_000000_create_users_table.php
+++ tests/TestSupport/database/0001_01_01_000000_create_users_table.php
@@ -12,7 +12,7 @@ class CreateUsersTable extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
@@ -29,7 +29,7 @@ class CreateUsersTable extends Migration
 
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
+            $table->unsignedBigInteger('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->longText('payload');
